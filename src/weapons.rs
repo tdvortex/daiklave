68901326ff42201bfa866bbfdd9bccf72ce8575a
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-11-25 11:17:28 -0800

    Add defense and overwhelming

--- src/weapons.rs
+++ src/weapons.rs
@@ -59,6 +59,7 @@ struct Weapon {
     handedness: Handedness,
     special_tags: HashSet<SpecialTag>,
 }
+
 #[derive(PartialEq, Eq, Hash, Debug)]
 enum AttackMethod {
     Brawl,
@@ -77,7 +78,8 @@ struct AttackMethods {
 
 impl AttackMethods {
     fn contains(&self, attack_method: &AttackMethod) -> bool {
-        self.default_attack_method == *attack_method || self.alternate_attack_methods.contains(attack_method)
+        self.default_attack_method == *attack_method
+            || self.alternate_attack_methods.contains(attack_method)
     }
 }
 
@@ -88,30 +90,45 @@ impl Weapon {
         }
 
         let base_accuracy = match (&self.weight_class, attack_method) {
-            (WeightClass::Light, AttackMethod::Brawl) 
-            | (WeightClass::Light, AttackMethod::Melee) 
+            (WeightClass::Light, AttackMethod::Brawl)
+            | (WeightClass::Light, AttackMethod::Melee)
             | (WeightClass::Light, AttackMethod::MartialArts(_)) => 4,
-            (WeightClass::Medium, AttackMethod::Brawl) 
-            | (WeightClass::Medium, AttackMethod::Melee) 
+            (WeightClass::Medium, AttackMethod::Brawl)
+            | (WeightClass::Medium, AttackMethod::Melee)
             | (WeightClass::Medium, AttackMethod::MartialArts(_)) => 2,
-            (WeightClass::Heavy, AttackMethod::Brawl) 
-            | (WeightClass::Heavy, AttackMethod::Melee) 
+            (WeightClass::Heavy, AttackMethod::Brawl)
+            | (WeightClass::Heavy, AttackMethod::Melee)
             | (WeightClass::Heavy, AttackMethod::MartialArts(_)) => 0,
-            (_, AttackMethod::Thrown(RangeBand::Close)) | (_, AttackMethod::MartialArtsThrown(_, RangeBand::Close)) => 4,
-            (_, AttackMethod::Thrown(RangeBand::Short)) | (_, AttackMethod::MartialArtsThrown(_, RangeBand::Short)) => 3,
-            (_, AttackMethod::Thrown(RangeBand::Medium)) | (_, AttackMethod::MartialArtsThrown(_, RangeBand::Medium)) => 2,
-            (_, AttackMethod::Thrown(RangeBand::Long)) | (_, AttackMethod::MartialArtsThrown(_, RangeBand::Long)) => -1,
-            (_, AttackMethod::Thrown(RangeBand::Extreme)) | (_, AttackMethod::MartialArtsThrown(_, RangeBand::Extreme)) => -3,
-            (_, AttackMethod::Archery(RangeBand::Close)) | (_, AttackMethod::MartialArtsArchery(_, RangeBand::Close)) => -2,
-            (_, AttackMethod::Archery(RangeBand::Short)) | (_, AttackMethod::MartialArtsArchery(_, RangeBand::Short)) => 4,
-            (_, AttackMethod::Archery(RangeBand::Medium)) | (_, AttackMethod::MartialArtsArchery(_, RangeBand::Medium)) => 2,
-            (_, AttackMethod::Archery(RangeBand::Long)) | (_, AttackMethod::MartialArtsArchery(_, RangeBand::Long)) => 0,
-            (_, AttackMethod::Archery(RangeBand::Extreme)) | (_, AttackMethod::MartialArtsArchery(_, RangeBand::Extreme)) => -2,
+            (_, AttackMethod::Thrown(RangeBand::Close))
+            | (_, AttackMethod::MartialArtsThrown(_, RangeBand::Close)) => 4,
+            (_, AttackMethod::Thrown(RangeBand::Short))
+            | (_, AttackMethod::MartialArtsThrown(_, RangeBand::Short)) => 3,
+            (_, AttackMethod::Thrown(RangeBand::Medium))
+            | (_, AttackMethod::MartialArtsThrown(_, RangeBand::Medium)) => 2,
+            (_, AttackMethod::Thrown(RangeBand::Long))
+            | (_, AttackMethod::MartialArtsThrown(_, RangeBand::Long)) => -1,
+            (_, AttackMethod::Thrown(RangeBand::Extreme))
+            | (_, AttackMethod::MartialArtsThrown(_, RangeBand::Extreme)) => -3,
+            (_, AttackMethod::Archery(RangeBand::Close))
+            | (_, AttackMethod::MartialArtsArchery(_, RangeBand::Close)) => -2,
+            (_, AttackMethod::Archery(RangeBand::Short))
+            | (_, AttackMethod::MartialArtsArchery(_, RangeBand::Short)) => 4,
+            (_, AttackMethod::Archery(RangeBand::Medium))
+            | (_, AttackMethod::MartialArtsArchery(_, RangeBand::Medium)) => 2,
+            (_, AttackMethod::Archery(RangeBand::Long))
+            | (_, AttackMethod::MartialArtsArchery(_, RangeBand::Long)) => 0,
+            (_, AttackMethod::Archery(RangeBand::Extreme))
+            | (_, AttackMethod::MartialArtsArchery(_, RangeBand::Extreme)) => -2,
         };
 
-        let exceptional_bonus = i8::from(self.quality == Quality::Exceptional || self.quality == Quality::Artifact);
-        let flame_bonus = match (self.special_tags.contains(&SpecialTag::Flame), attack_method) {
-            (true, AttackMethod::Archery(RangeBand::Close)) | (true, AttackMethod::MartialArtsArchery(_, RangeBand::Close)) => 2,
+        let exceptional_bonus =
+            i8::from(self.quality == Quality::Exceptional || self.quality == Quality::Artifact);
+        let flame_bonus = match (
+            self.special_tags.contains(&SpecialTag::Flame),
+            attack_method,
+        ) {
+            (true, AttackMethod::Archery(RangeBand::Close))
+            | (true, AttackMethod::MartialArtsArchery(_, RangeBand::Close)) => 2,
             (_, _) => 0,
         };
 
@@ -119,8 +136,12 @@ impl Weapon {
     }
 
     fn damage(&self, attack_method: &AttackMethod) -> i8 {
-        let effective_weight = match (self.special_tags.contains(&SpecialTag::Powerful), attack_method) {
-            (true, AttackMethod::Archery(RangeBand::Close)) | (true, AttackMethod::MartialArtsArchery(_, RangeBand::Close)) => &WeightClass::Heavy,
+        let effective_weight = match (
+            self.special_tags.contains(&SpecialTag::Powerful),
+            attack_method,
+        ) {
+            (true, AttackMethod::Archery(RangeBand::Close))
+            | (true, AttackMethod::MartialArtsArchery(_, RangeBand::Close)) => &WeightClass::Heavy,
             (_, _) => &self.weight_class,
         };
 
@@ -132,11 +153,49 @@ impl Weapon {
 
         let artifact_bonus = 3 * i8::from(self.quality == Quality::Artifact);
 
-        let shield_penalty = match (self.special_tags.contains(&SpecialTag::Shield), attack_method) {
-                (true, AttackMethod::Brawl) | (true, AttackMethod::Melee) | (true, AttackMethod::MartialArts(_)) => -2,
-                _ => 0
-            };
+        let shield_penalty = match (
+            self.special_tags.contains(&SpecialTag::Shield),
+            attack_method,
+        ) {
+            (true, AttackMethod::Brawl)
+            | (true, AttackMethod::Melee)
+            | (true, AttackMethod::MartialArts(_)) => -2,
+            _ => 0,
+        };
 
-        base_damage + artifact_bonus + shield_penalty       
+        base_damage + artifact_bonus + shield_penalty
+    }
+
+    fn overwhelming(&self) -> i8 {
+        let balanced_bonus = 2 * i8::from(self.special_tags.contains(&SpecialTag::Balanced));
+
+        let base_overwhelming = match (&self.quality, &self.weight_class) {
+            (Quality::Artifact, WeightClass::Light) => 3,
+            (Quality::Artifact, WeightClass::Medium) => 4,
+            (Quality::Artifact, WeightClass::Heavy) => 5,
+            (_, _) => 1,
+        };
+
+        base_overwhelming + balanced_bonus
+    }
+
+    fn attunement(&self) -> i8 {
+        5 * i8::from(self.quality == Quality::Artifact)
+    }
+
+    fn defense(&self) -> Option<i8> {
+        match (
+            &self.attack_methods.default_attack_method,
+            &self.weight_class,
+            &self.quality,
+        ) {
+            (AttackMethod::Archery(_), _, _)
+            | (AttackMethod::MartialArtsArchery(_, _), _, _)
+            | (AttackMethod::Thrown(_), _, _)
+            | (AttackMethod::MartialArtsThrown(_, _), _, _) => None,
+            (_, WeightClass::Light, _) | (_, WeightClass::Heavy, Quality::Artifact) => Some(0),
+            (_, WeightClass::Medium, _) => Some(1),
+            (_, WeightClass::Heavy, _) => Some(-1),
+        }
     }
 }
