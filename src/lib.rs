92dd72f0802e4605236010e77812d1a3cd44f9af
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-11-25 17:31:23 -0800

    Export Character trait and MortalCharacter struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,15 @@ mod character;
 mod mortal;
 mod range_bands;
 
+pub use crate::mortal::MortalCharacter;
+pub use crate::character::Character;
+
 #[cfg(test)]
 mod tests {
+    use super::MortalCharacter;
+
     #[test]
     fn default_mortal() {
-        use crate::mortal::MortalCharacter;
         let character = MortalCharacter::default();
         dbg!(character);
     }
