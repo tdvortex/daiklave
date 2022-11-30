b7fa12bbf3b0913b751a3aaf66f9acc13615c0d1
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-11-29 17:17:41 -0800

    Add remove campaign

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,7 +1,7 @@
 use eyre::{Result};
 use sqlx::{query, query_as, PgPool};
 
-struct Campaign {
+pub struct Campaign {
     id: i64,
     name: String,
     description: Option<String>,
@@ -9,7 +9,7 @@ struct Campaign {
 }
 
 
-async fn get_campaign(pool: &PgPool, id: i64) -> Result<Campaign> {
+pub async fn get_campaign(pool: &PgPool, id: i64) -> Result<Campaign> {
     let campaign = query_as!(
         Campaign,
         "SELECT * FROM campaigns WHERE id = $1",
@@ -19,7 +19,7 @@ async fn get_campaign(pool: &PgPool, id: i64) -> Result<Campaign> {
     Ok(campaign)
 }
 
-async fn create_campaign(pool: &PgPool, name: String, bot_channel: i64, maybe_description: Option<String>) -> Result<i64> {
+pub async fn create_campaign(pool: &PgPool, name: String, bot_channel: i64, maybe_description: Option<String>) -> Result<i64> {
     let id = query!(
         "INSERT INTO campaigns(name, bot_channel, description) VALUES ($1, $2, $3) RETURNING id",
         name,
@@ -28,4 +28,13 @@ async fn create_campaign(pool: &PgPool, name: String, bot_channel: i64, maybe_de
     ).fetch_one(pool).await?.id;
 
     Ok(id)
+}
+
+pub async fn remove_campaign(pool: &PgPool, id: i64) -> Result<()> {
+    let _ = query!(
+        "DELETE FROM campaigns WHERE id = $1",
+        id
+    ).execute(pool).await?;
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 pub use character::mortal::MortalCharacter;
 pub mod character;
-mod database;
+pub mod database;
