22b54ffaf6c3d1519b846dc7e77a30a2533d742b
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-11-29 17:30:25 -0800

    Add player

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -39,3 +39,38 @@ impl Campaign {
         Ok(())
     }
 }
+
+pub struct Player {
+    pub id: i64,
+    pub name: String,
+}
+
+impl Player {
+    pub async fn get(pool: &PgPool, id: i64) -> Result<Player> {
+        let player = query_as!(
+            Player,
+            "SELECT * FROM players WHERE id = $1",
+            id
+        ).fetch_one(pool).await?;
+    
+        Ok(player)
+    }
+
+    pub async fn create(pool: &PgPool, name: String) -> Result<i64> {
+        let id = query!(
+            "INSERT INTO players(name) VALUES ($1) RETURNING id",
+            name
+        ).fetch_one(pool).await?.id;
+    
+        Ok(id)
+    }
+
+    pub async fn remove(pool: &PgPool, id: i64) -> Result<()> {
+        query!(
+            "DELETE FROM players WHERE id = $1",
+            id
+        ).execute(pool).await?;
+    
+        Ok(())
+    }
+}
