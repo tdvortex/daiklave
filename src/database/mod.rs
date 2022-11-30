2d5bf16a9d3476fdadb8ba92d7ff41bfca2ae5b9
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-11-29 17:25:13 -0800

    Change to static methods

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -8,33 +8,34 @@ pub struct Campaign {
     bot_channel: i64,
 }
 
-
-pub async fn get_campaign(pool: &PgPool, id: i64) -> Result<Campaign> {
-    let campaign = query_as!(
-        Campaign,
-        "SELECT * FROM campaigns WHERE id = $1",
-        id
-    ).fetch_one(pool).await?;
-
-    Ok(campaign)
-}
-
-pub async fn create_campaign(pool: &PgPool, name: String, bot_channel: i64, maybe_description: Option<String>) -> Result<i64> {
-    let id = query!(
-        "INSERT INTO campaigns(name, bot_channel, description) VALUES ($1, $2, $3) RETURNING id",
-        name,
-        bot_channel,
-        maybe_description
-    ).fetch_one(pool).await?.id;
-
-    Ok(id)
-}
-
-pub async fn remove_campaign(pool: &PgPool, id: i64) -> Result<()> {
-    let _ = query!(
-        "DELETE FROM campaigns WHERE id = $1",
-        id
-    ).execute(pool).await?;
-
-    Ok(())
+impl Campaign {
+    pub async fn get(pool: &PgPool, id: i64) -> Result<Campaign> {
+        let campaign = query_as!(
+            Campaign,
+            "SELECT * FROM campaigns WHERE id = $1",
+            id
+        ).fetch_one(pool).await?;
+    
+        Ok(campaign)
+    }
+    
+    pub async fn create(pool: &PgPool, name: String, bot_channel: i64, maybe_description: Option<String>) -> Result<i64> {
+        let id = query!(
+            "INSERT INTO campaigns(name, bot_channel, description) VALUES ($1, $2, $3) RETURNING id",
+            name,
+            bot_channel,
+            maybe_description
+        ).fetch_one(pool).await?.id;
+    
+        Ok(id)
+    }
+    
+    pub async fn remove(pool: &PgPool, id: i64) -> Result<()> {
+        query!(
+            "DELETE FROM campaigns WHERE id = $1",
+            id
+        ).execute(pool).await?;
+    
+        Ok(())
+    }
 }
