91922296eea22b1482e0270933df8e420aa5516e
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-12-02 18:34:27 -0800

    Refactor to remove redundant character_id

--- src/database/get_character.sql
+++ src/database/get_character.sql
@@ -1,6 +1,5 @@
 WITH player_campaign_query AS (
     SELECT
-        characters.id as character_id,
         players AS player,
         campaigns AS campaign
     FROM characters
@@ -12,102 +11,79 @@ WITH player_campaign_query AS (
 ),
 attributes_query AS (
     SELECT
-        characters.id as character_id,
         ARRAY_AGG(attributes) AS attrs
     FROM characters
         INNER JOIN attributes ON (attributes.character_id = characters.id)
     WHERE characters.id = $1
-    GROUP BY 1
 ), abilities_query AS (
     SELECT
-        characters.id as character_id,
         ARRAY_AGG(abilities) AS abils
     FROM characters
         INNER JOIN abilities ON (abilities.character_id = characters.id)
     WHERE characters.id = $1
-    GROUP BY 1
 ), specialties_query AS (
     SELECT
-        characters.id as character_id,
         ARRAY_AGG(specialties) AS specs
     FROM characters
         INNER JOIN abilities ON (abilities.character_id = characters.id)
         INNER JOIN specialties ON (specialties.ability_id = abilities.id)
     WHERE characters.id = $1
-    GROUP BY 1
 ), 
 intimacies_query AS (
     SELECT
-        characters.id as character_id,
         ARRAY_AGG(intimacies) AS intis
     FROM characters
         INNER JOIN intimacies ON (intimacies.character_id = characters.id)
     WHERE characters.id = $1
-    GROUP BY 1
 ), health_boxes_query AS (
     SELECT
-        characters.id as character_id,
         ARRAY_AGG(health_boxes) AS hboxs
     FROM characters
         INNER JOIN health_boxes ON (health_boxes.character_id = characters.id)
     WHERE characters.id = $1
-    GROUP BY 1
 ), weapons_query AS (
      SELECT
-        characters.id as character_id,
         ARRAY_AGG(weapons) as weaps
     FROM characters
         INNER JOIN character_weapons ON (characters.id = character_weapons.character_id)
         INNER JOIN weapons ON (character_weapons.weapon_id = weapons.id)
     WHERE characters.id = $1
-    GROUP BY 1
 ), weapons_equipped_query AS (
     SELECT
-        characters.id as character_id,
         ARRAY_AGG(character_weapons) AS eqwps
     FROM characters
         INNER JOIN character_weapons ON (character_weapons.character_id = characters.id)
     WHERE characters.id = $1 AND character_weapons.equip_hand IS NOT NULL
-    GROUP BY 1
 ), armor_query AS (
      SELECT
-        characters.id as character_id,
         ARRAY_AGG(armor) as armrs
     FROM characters
         INNER JOIN character_armor ON (characters.id = character_armor.character_id)
         INNER JOIN armor ON (character_armor.armor_id = armor.id)
     WHERE characters.id = $1
-    GROUP BY 1
 ), armor_worn_query AS (
      SELECT
-        characters.id as character_id,
         ARRAY_AGG(character_armor) as wrars
     FROM characters
         INNER JOIN character_armor ON (characters.id = character_armor.character_id)
     WHERE characters.id = $1 AND character_armor.worn
-    GROUP BY 1
 ), merits_query AS (
     SELECT
-        characters.id AS character_id,
         ARRAY_AGG(merits) AS merts
     FROM characters 
         INNER JOIN character_merits ON (characters.id = character_merits.character_id)
         INNER JOIN merits ON (merits.id = character_merits.merit_id)
     WHERE characters.id = $1
-    GROUP BY 1
 ), merit_prerequisite_sets_query AS (
     SELECT
-        characters.id AS character_id,
         ARRAY_AGG(merit_prerequisite_sets) AS mprss
     FROM characters
         INNER JOIN character_merits ON (characters.id = character_merits.character_id)
         INNER JOIN merits ON (merits.id = character_merits.merit_id)
         INNER JOIN merit_prerequisite_sets ON (merit_prerequisite_sets.merit_id = merits.id)
     WHERE characters.id = $1
-    GROUP BY 1
 ), merit_prerequisites_query AS (
     SELECT
-        characters.id AS character_id,
         ARRAY_AGG(prerequisites) AS meprs
     FROM characters
         INNER JOIN character_merits ON (characters.id = character_merits.character_id)
@@ -115,7 +91,6 @@ intimacies_query AS (
         INNER JOIN merit_prerequisite_sets ON (merit_prerequisite_sets.merit_id = merits.id)
         INNER JOIN prerequisites ON (merit_prerequisite_sets.prerequisite_id = prerequisites.id)
     WHERE characters.id = $1
-    GROUP BY 1
 )
 SELECT
     characters AS "character!: CharacterRow",
@@ -133,18 +108,18 @@ SELECT
     merts AS "merits: Vec<MeritRow>",
     mprss AS "merit_prerequisite_sets: Vec<MeritPrerequisiteSetRow>",
     meprs AS "merit_prerequisites:  Vec<PrerequisiteRow>"
-FROM characters
-    INNER JOIN player_campaign_query ON (characters.id = character_id)
-    INNER JOIN attributes_query USING (character_id)
-    INNER JOIN abilities_query USING (character_id)
-    INNER JOIN health_boxes_query USING (character_id)
-    INNER JOIN weapons_query USING (character_id)
-    LEFT JOIN specialties_query USING (character_id)
-    LEFT JOIN intimacies_query USING (character_id)
-    LEFT JOIN weapons_equipped_query USING (character_id)
-    LEFT JOIN armor_query USING (character_id)
-    LEFT JOIN armor_worn_query USING (character_id)
-    LEFT JOIN merits_query USING (character_id)
-    LEFT JOIN merit_prerequisite_sets_query USING (character_id)
-    LEFT JOIN merit_prerequisites_query USING (character_id)
+FROM characters,
+    player_campaign_query,
+    attributes_query,
+    abilities_query,
+    health_boxes_query,
+    weapons_query,
+    specialties_query,
+    intimacies_query,
+    weapons_equipped_query,
+    armor_query,
+    armor_worn_query,
+    merits_query,
+    merit_prerequisite_sets_query,
+    merit_prerequisites_query
 WHERE characters.id = $1;
