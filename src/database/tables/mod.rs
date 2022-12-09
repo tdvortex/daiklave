99a3e361a3679683ed0884149754dedd92031104
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-12-09 14:42:41 -0800

    Relocate charms

--- src/database/tables/mod.rs
+++ src/database/tables/mod.rs
@@ -1,4 +1,3 @@
 pub mod character;
-pub mod charms;
 pub mod players;
 pub mod prerequisites;
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod armor;
 pub mod attributes;
 pub mod campaign;
 pub mod character;
+pub mod charms;
 pub mod health;
 pub mod intimacies;
 pub mod merits;
