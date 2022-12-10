80784333caf3bd06095ef3767f781ec5ff2d6ba6
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-12-09 19:10:26 -0800

    Test initial serialization/deserialization

--- tests/lifecycle.rs
+++ tests/lifecycle.rs
@@ -1,7 +1,22 @@
-#[test]
+use postcard::from_bytes;
+use sqlx::postgres::PgPool;
+use exalted_3e_gui::create_player;
+
+#[sqlx::test]
 fn lifecycle() {
+    dotenvy::dotenv().unwrap();
+    let url = dotenvy::var("DATABASE_URL").unwrap();
+    let _pool = PgPool::connect(&url).await.unwrap();
+
     // User inputs a username, Client serializes it
+    let player_name = "Ralph Waldo Emerson".to_owned();
+    let send_bytes = postcard::to_allocvec(&player_name).unwrap();
+    
     // Server deserializes it and creates a new player
+    let receive_name: String = from_bytes(&send_bytes).unwrap();
+    assert_eq!(receive_name, player_name);
+
+
     // Server serializes player result and sends it back to the client
     // Client deserializes and extracts player ID
     // Client (in isolation) creates a character and subcomponents
