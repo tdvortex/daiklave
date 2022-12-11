ced6cca5165e4c5debed6ad28cfe6266e4cc62a2
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-12-11 11:37:44 -0800

    Add modify base player (passing)

--- tests/fixtures/character.rs
+++ tests/fixtures/character.rs
@@ -37,3 +37,33 @@ pub fn validate_initial_base_character(
     assert_eq!(character.experience.current, 15);
     assert_eq!(character.experience.total, 15);
 }
+
+pub fn modify_base_character(character: &mut Character) {
+    // Erase character concept
+    character.concept = None;
+
+    // Give him a better name
+    character.name = "Incomparable Wanderer".to_owned();
+
+    // Update willpower
+    character.willpower.maximum = 7;
+    character.willpower.recover_all();
+
+    // Update experience
+    character.experience.total += 5;
+    character.experience.current -= 5;
+}
+
+pub fn validate_modified_base_character(
+    player: &Player,
+    character: &Character,
+) {
+    assert_eq!(character.player(), player);
+    assert_eq!(character.name.as_str(), "Incomparable Wanderer");
+    assert!(character.campaign().is_none());
+    assert!(character.concept.is_none());
+    assert_eq!(character.willpower.current, 7);
+    assert_eq!(character.willpower.maximum, 7);
+    assert_eq!(character.experience.current, 10);
+    assert_eq!(character.experience.total, 20);
+}
--- tests/fixtures/mod.rs
+++ tests/fixtures/mod.rs
@@ -19,7 +19,7 @@ use self::{
     abilities::{create_intitial_abilities, validate_initial_abilities, modify_abilities, validate_modified_abilities},
     armor::{create_initial_armor, validate_initial_armor_items},
     attributes::{create_initial_attributes, validate_initial_attributes, modify_attributes, validate_modified_attributes},
-    character::create_initial_base_character,
+    character::{create_initial_base_character, modify_base_character, validate_modified_base_character},
     health::{create_initial_health, validate_initial_health, modify_health, validate_modified_health},
     intimacies::{create_initial_intimacies, validate_initial_intimacies},
     merits::{create_initial_merits, validate_initial_merits},
@@ -55,15 +55,17 @@ pub fn validate_initial_character(
 }
 
 pub fn modify_character(character: &mut Character) {
+    modify_base_character(character);
     modify_abilities(&mut character.abilities);
     modify_attributes(&mut character.attributes);
     modify_health(&mut character.health);
 }
 
 pub fn validate_modified_character(
-    _player: &Player,
+    player: &Player,
     modified_character: &Character,
 ) {
+    validate_modified_base_character(&player, &modified_character);
     validate_modified_abilities(&modified_character.abilities);
     validate_modified_attributes(&modified_character.attributes);
     validate_modified_health(&modified_character.health);
