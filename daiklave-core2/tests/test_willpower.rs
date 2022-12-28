91124dfab51c1149da8713169148ee31d588d312
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-12-28 08:29:08 -0800

    (RED) EventSource willpower test

--- daiklave-core2/tests/test_willpower.rs
+++ daiklave-core2/tests/test_willpower.rs
@@ -1,4 +1,4 @@
-use daiklave_core2::{Character, CharacterView, SolarTraits};
+use daiklave_core2::{Character, CharacterView, SolarTraits, CharacterEventSource, CharacterMutation};
 
 #[test]
 fn test_willpower_character() {
@@ -50,4 +50,50 @@ fn test_willpower_character_view() {
     assert!(character_view.set_willpower_rating(7).is_ok());
     assert_eq!(character_view.willpower().rating(), 7);
     assert_eq!(character_view.willpower().current(), 7);
+}
+
+#[test]
+fn test_willpower_character_event_source() {
+        // Check default (mortal)
+        let mut event_source = CharacterEventSource::default();
+        let character_view = event_source.as_character_view().unwrap();
+        assert_eq!(character_view.willpower().rating(), 3);
+        assert_eq!(character_view.willpower().current(), 3);
+    
+        // Check default (exalt)
+        let solar_traits = SolarTraits::builder().build();
+        let mutation = CharacterMutation::SetSolar(solar_traits);
+        event_source.apply_mutation(mutation).unwrap();
+        let character_view = event_source.as_character_view().unwrap();
+        assert_eq!(character_view.willpower().rating(), 5);
+        assert_eq!(character_view.willpower().current(), 5);
+    
+        // Check modifying current willpower
+        let mutation = CharacterMutation::SetCurrentWillpower(3);
+        assert!(character_view.check_mutation(&mutation).is_ok());
+        assert!(event_source.apply_mutation(mutation).is_ok());
+        let character_view = event_source.as_character_view().unwrap();
+        assert_eq!(character_view.willpower().rating(), 5);
+        assert_eq!(character_view.willpower().current(), 3);
+    
+        // Check modifying willpower rating
+        let mutation = CharacterMutation::SetWillpowerRating(7);
+        assert!(character_view.check_mutation(&mutation).is_ok());
+        event_source.apply_mutation(mutation).unwrap();
+        let character_view = event_source.as_character_view().unwrap();
+        assert_eq!(character_view.willpower().rating(), 7);
+        assert_eq!(character_view.willpower().current(), 7);
+
+        // Check we can undo the full history
+        assert!(!event_source.can_redo());
+        assert!(event_source.undo());
+        assert!(event_source.undo());
+        assert!(event_source.undo());
+        assert!(!event_source.can_undo());
+
+        // Check we can redo the full history
+        assert!(event_source.redo());
+        assert!(event_source.redo());
+        assert!(event_source.redo());
+        assert!(!event_source.can_redo());
 }
