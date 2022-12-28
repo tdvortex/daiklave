1b842c7e44a2c8ae0a9c754e2369559752761e6d
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-12-28 10:59:07 -0800

    (RED) Character attributes test

---
+++ daiklave-core2/tests/test_attributes.rs
@@ -0,0 +1,25 @@
+use daiklave_core2::Character;
+
+#[test]
+fn test_attributes_character() {
+    // Check default attributes
+    let mut character = Character::default();
+    assert_eq!(character.attributes().get_dots(AttributeName::Strength), 1);
+    assert_eq!(character.attributes().get_dots(AttributeName::Dexterity), 1);
+    assert_eq!(character.attributes().get_dots(AttributeName::Stamina), 1);
+    assert_eq!(character.attributes().get_dots(AttributeName::Charisma), 1);
+    assert_eq!(character.attributes().get_dots(AttributeName::Manipulation), 1);
+    assert_eq!(character.attributes().get_dots(AttributeName::Appearance), 1);
+    assert_eq!(character.attributes().get_dots(AttributeName::Perception), 1);
+    assert_eq!(character.attributes().get_dots(AttributeName::Intelligence), 1);
+    assert_eq!(character.attributes().get_dots(AttributeName::Wits), 1);
+
+    // Check setting attributes
+    assert!(character.check_set_attribute(AttributeName::Strength, 2).is_ok());
+    assert!(character.set_attribute(AttributeName::Strength, 2).is_ok());
+    assert_eq!(character.attributes().get_dots(AttributeName::Strength), 2);
+
+    // Check out-of-bounds prevention
+    assert!(character.check_set_attribute(AttributeName::Dexterity, 0).is_err());
+    assert!(character.check_set_attribute(AttributeName::Dexterity, 6).is_err());
+}
