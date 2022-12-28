cfc7f49e7579bfadd0bb1dcc664b802cbc29f523
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-12-28 08:18:02 -0800

    (RED) CharacterView willpower test

--- daiklave-core2/tests/test_willpower.rs
+++ daiklave-core2/tests/test_willpower.rs
@@ -1,4 +1,4 @@
-use daiklave_core2::{Character, SolarTraits};
+use daiklave_core2::{Character, SolarTraits, CharacterView};
 
 #[test]
 fn test_willpower_character() {
@@ -24,4 +24,30 @@ fn test_willpower_character() {
     assert!(character.set_willpower_rating(7).is_ok());
     assert_eq!(character.willpower().rating(), 7);
     assert_eq!(character.willpower().current(), 7);
+}
+
+#[test]
+fn test_willpower_character_view() {
+    // Check default (mortal)
+    let mut character_view = CharacterView::default();
+    assert_eq!(character_view.willpower().rating(), 3);
+    assert_eq!(character_view.willpower().current(), 3);
+
+    // Check default (exalt)
+    let solar_traits = SolarTraits::builder().build();
+    character_view.set_solar(&solar_traits).unwrap();
+    assert_eq!(character_view.willpower().rating(), 5);
+    assert_eq!(character_view.willpower().current(), 5);
+
+    // Check modifying current willpower
+    assert!(character_view.check_set_current_willpower(3).is_ok());
+    assert!(character_view.set_current_willpower(3).is_ok());
+    assert_eq!(character_view.willpower().rating(), 5);
+    assert_eq!(character_view.willpower().current(), 3);
+
+    // Check modifying willpower rating
+    assert!(character_view.check_set_willpower_rating(7).is_ok());
+    assert!(character_view.set_willpower_rating(7).is_ok());
+    assert_eq!(character_view.willpower().rating(), 7);
+    assert_eq!(character_view.willpower().current(), 7);
 }
