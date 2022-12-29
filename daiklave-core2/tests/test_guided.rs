f493d7267b5c9bc840765b9e78948a030f0965db
Ben Wilson <ben.aiken.wilson@gmail.com>
2022-12-28 18:25:54 -0800

    (RED) Add Solar builder (through attributes)

--- daiklave-core2/tests/test_guided.rs
+++ daiklave-core2/tests/test_guided.rs
@@ -98,3 +98,98 @@ fn test_guided_mortal() {
     guided_builder.undo();
     assert_eq!(guided_builder.bonus_points_remaining(), 21);
 }
+
+#[test]
+fn test_guided_solar() {
+    let mut guided_builder = begin_guided_builder(CharacterId(Id::Placeholder(1)));
+
+    // Choose character name
+    let mutation = GuidedCharacterMutation::CharacterMutation(CharacterMutation::SetName(
+        "Test Solar".to_owned(),
+    ));
+    assert!(guided_builder.check_mutation(&mutation).is_ok());
+    assert!(guided_builder.apply_mutation(mutation).is_ok());
+
+    // Choose character concept
+    let mutation = GuidedCharacterMutation::CharacterMutation(CharacterMutation::SetConcept(
+        "Test Concept".to_owned(),
+    ));
+    assert!(guided_builder.check_mutation(&mutation).is_ok());
+    assert!(guided_builder.apply_mutation(mutation).is_ok());
+
+    // Move on to next stage
+    let mutation = GuidedCharacterMutation::SetStage(GuidedStage::ChooseExaltation);
+    assert!(guided_builder.check_mutation(&mutation).is_ok());
+    assert!(guided_builder.apply_mutation(mutation).is_ok());
+
+    // Move back to previous stage and undo adding concept
+    assert!(guided_builder.can_undo());
+    assert!(guided_builder.undo());
+    assert!(guided_builder.can_undo());
+    assert!(guided_builder.undo());
+
+    // Move on to next stage (again)
+    let mutation = GuidedCharacterMutation::SetStage(GuidedStage::ChooseExaltation);
+    assert!(guided_builder.check_mutation(&mutation).is_ok());
+    assert!(guided_builder.apply_mutation(mutation).is_ok());
+
+    // Bonus points are not alloted until after choosing exaltation
+    assert_eq!(guided_builder.bonus_points_remaining(), 0);
+
+    // Choose to be mortal and progress to attributes
+    let mutation = GuidedCharacterMutation::SetExaltation(ExaltationChoice::Night);
+    assert!(guided_builder.check_mutation(&mutation).is_ok());
+    assert!(guided_builder.apply_mutation(mutation).is_ok());
+
+    let mutation = GuidedCharacterMutation::SetStage(GuidedStage::ChooseAttributes);
+    assert!(guided_builder.check_mutation(&mutation).is_ok());
+    assert!(guided_builder.apply_mutation(mutation).is_ok());
+
+    assert_eq!(guided_builder.bonus_points_remaining(), 15);
+
+    // Set attributes without bonus points
+    [
+        CharacterMutation::SetAttribute(AttributeName::Strength, 2),
+        CharacterMutation::SetAttribute(AttributeName::Dexterity, 5),
+        CharacterMutation::SetAttribute(AttributeName::Stamina, 2),
+        CharacterMutation::SetAttribute(AttributeName::Charisma, 2),
+        CharacterMutation::SetAttribute(AttributeName::Manipulation, 3),
+        CharacterMutation::SetAttribute(AttributeName::Appearance, 2),
+        CharacterMutation::SetAttribute(AttributeName::Perception, 4),
+        CharacterMutation::SetAttribute(AttributeName::Intelligence, 4),
+        CharacterMutation::SetAttribute(AttributeName::Wits, 3),
+    ]
+    .into_iter()
+    .map(|cm| GuidedCharacterMutation::CharacterMutation(cm))
+    .for_each(|gcm| assert!(guided_builder.apply_mutation(gcm).is_ok()));
+
+    assert_eq!(guided_builder.bonus_points_remaining(), 15);
+
+    // Check attribute bonus points costs
+    guided_builder
+        .apply_mutation(GuidedCharacterMutation::CharacterMutation(
+            CharacterMutation::SetAttribute(AttributeName::Stamina, 3),
+        ))
+        .unwrap();
+    assert_eq!(guided_builder.bonus_points_remaining(), 11);
+
+    guided_builder
+        .apply_mutation(GuidedCharacterMutation::CharacterMutation(
+            CharacterMutation::SetAttribute(AttributeName::Wits, 4),
+        ))
+        .unwrap();
+    assert_eq!(guided_builder.bonus_points_remaining(), 7);
+
+    guided_builder
+        .apply_mutation(GuidedCharacterMutation::CharacterMutation(
+            CharacterMutation::SetAttribute(AttributeName::Appearance, 3),
+        ))
+        .unwrap();
+    assert_eq!(guided_builder.bonus_points_remaining(), 4);
+
+    // Revert attribute bonus point expenditures
+    guided_builder.undo();
+    guided_builder.undo();
+    guided_builder.undo();
+    assert_eq!(guided_builder.bonus_points_remaining(), 15);
+}
