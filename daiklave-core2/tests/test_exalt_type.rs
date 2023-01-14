19e35eb9915e359d91db57ac7f77515afd4a7d5d
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-01-14 06:23:27 -0800

    Rewrite Solar builder test

--- daiklave-core2/tests/test_exalt_type.rs
+++ daiklave-core2/tests/test_exalt_type.rs
@@ -1,105 +1,35 @@
 use daiklave_core2::{
     abilities::AbilityName,
-    exaltation::exalt::exalt_type::solar::{caste::twilight::Twilight, Solar},
-    Character, CharacterEventSource, CharacterMutation,
+    exaltation::exalt::exalt_type::solar::{Solar},
+    CharacterEventSource, CharacterMutation,
 };
 
 #[test]
-fn test_exalt_type_character_view() {
-    // Confirm default is mortal
-    let mut character_view = Character::default();
-    assert!(character_view.is_mortal());
-
-    // Confirm toggle to solar
-    let twilight = {
-        let mut builder = Twilight::builder();
-        [
-            AbilityName::Bureaucracy,
-            AbilityName::Craft,
-            AbilityName::Integrity,
-            AbilityName::Investigation,
-        ]
-        .into_iter()
-        .for_each(|ability| {
-            builder.add_caste_ability(ability).unwrap();
-        });
-        builder
-            .set_supernal_ability(AbilityName::Linguistics)
-            .unwrap();
-        builder.build().unwrap()
-    };
-
-    let solar_traits = {
-        let mut builder = Solar::builder();
-        builder.set_twilight(twilight);
-        [
-            AbilityName::Archery,
-            AbilityName::Athletics,
-            AbilityName::Awareness,
-            AbilityName::Brawl,
-            AbilityName::Dodge,
-        ]
-        .into_iter()
-        .for_each(|ability| {
-            builder.add_favored_ability(ability).unwrap();
-        });
-        builder.build_view().unwrap().as_memo()
-    };
-
-    character_view.check_set_solar(&solar_traits).unwrap();
-    character_view.set_solar(&solar_traits).unwrap();
-    assert!(character_view.is_solar());
-
-    // Confirm toggle to mortal
-    character_view.check_set_mortal().unwrap();
-    character_view.set_mortal().unwrap();
-    assert!(character_view.is_mortal());
-}
-
-#[test]
-fn test_exalt_type_character_event_source() {
+fn test_exalt_type() {
     // Check default is mortal
     let mut event_source = CharacterEventSource::default();
     let character_view = event_source.as_character_view().unwrap();
     assert!(character_view.is_mortal());
 
-    // Check toggle to solar
-    let twilight = {
-        let mut builder = Twilight::builder();
-        [
-            AbilityName::Bureaucracy,
-            AbilityName::Craft,
-            AbilityName::Integrity,
-            AbilityName::Investigation,
-        ]
-        .into_iter()
-        .for_each(|ability| {
-            builder.add_caste_ability(ability).unwrap();
-        });
-        builder
-            .set_supernal_ability(AbilityName::Linguistics)
-            .unwrap();
-        builder.build().unwrap()
-    };
+    // Confirm toggle to solar
+    let add_solar = Solar::builder()
+        .twilight()
+        .caste_ability(TwilightAbility::Bureaucracy)
+        .caste_ability(TwilightAbility::Craft)
+        .caste_ability(TwilightAbility::Integrity)
+        .caste_ability(TwilightAbility::Investigation)
+        .caste_ability(TwilightAbility::Linguistics)
+        .supernal_ability(TwilightAbility::Linguistics)
+        .favored_ability(AbilityName::Archery)
+        .favored_ability(AbilityName::Athletics)
+        .favored_ability(AbilityName::Awareness)
+        .favored_ability(AbilityName::Brawl)
+        .favored_ability(AbilityName::Dodge)
+        .limit_trigger("Being told you're wrong when you're not".to_owned())
+        .build()
+        .unwrap();
 
-    let solar_traits = {
-        let mut builder = Solar::builder();
-        builder.set_twilight(twilight);
-        [
-            AbilityName::Archery,
-            AbilityName::Athletics,
-            AbilityName::Awareness,
-            AbilityName::Brawl,
-            AbilityName::Dodge,
-        ]
-        .into_iter()
-        .for_each(|ability| {
-            builder.add_favored_ability(ability).unwrap();
-        });
-        builder.build_view().unwrap().as_memo()
-    };
-
-    let mutation = CharacterMutation::SetSolar(Box::new(solar_traits));
+    let mutation = CharacterMutation::SetSolar(add_solar);
     character_view.check_mutation(&mutation).unwrap();
     event_source.apply_mutation(mutation).unwrap();
     let character_view = event_source.as_character_view().unwrap();
