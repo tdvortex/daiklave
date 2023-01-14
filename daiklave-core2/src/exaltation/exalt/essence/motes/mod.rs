1b42379466c368540807446dbd9a8ef47eb3b8a4
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-01-14 05:29:16 -0800

    Add artifact attunement iter

--- daiklave-core2/src/exaltation/exalt/essence/motes/mod.rs
+++ daiklave-core2/src/exaltation/exalt/essence/motes/mod.rs
@@ -1,4 +1,9 @@
-use crate::exaltation::exalt::{ExaltArmor, ExaltWeapons, ExaltWonders};
+use crate::{
+    armor::armor_item::{ArmorType, ArmorWeightClass},
+    artifact::ArtifactId,
+    exaltation::exalt::{ExaltArmor, ExaltWeapons, ExaltWonders},
+    weapons::weapon::WeaponType,
+};
 
 mod state;
 pub(crate) use state::{MotesState, MotesStateMemo};
@@ -26,7 +31,94 @@ impl<'view, 'source> Motes<'view, 'source> {
 
     /// All effects the Exalt has currently committed motes to (including
     /// artifact attunement)
-    pub fn committed(&self) -> impl Iterator<Item = (MoteCommitmentId, MoteCommitment<'source>)> {
-        vec![].into_iter()
+    pub fn committed(
+        &self,
+    ) -> impl Iterator<Item = (MoteCommitmentId, MoteCommitment<'source>)> + '_ {
+        let other_commitments = self.state.commitments.iter().map(|(k, v)| (*k, *v));
+
+        let weapon_commitments =
+            self.weapons.iter().filter_map(|(weapon_id, equipped)| {
+                match self
+                    .weapons
+                    .get_weapon(weapon_id, equipped)
+                    .map(|weapon| weapon.0)
+                {
+                    None => None,
+                    Some(WeaponType::Unarmed) => None,
+                    Some(WeaponType::Mundane(_, _, _)) => None,
+                    Some(WeaponType::Artifact(artifact_weapon_id, weapon, attunement)) => {
+                        if let Some(personal) = attunement {
+                            Some((
+                                MoteCommitmentId::AttunedArtifact(ArtifactId::Weapon(
+                                    artifact_weapon_id,
+                                )),
+                                MoteCommitment {
+                                    name: weapon.name(),
+                                    peripheral: 5 - personal.min(5),
+                                    personal: personal.min(5),
+                                },
+                            ))
+                        } else {
+                            None
+                        }
+                    }
+                }
+            });
+
+        let armor_commitments = self.armor.iter().filter_map(|armor_id| {
+            self.armor
+                .get(armor_id)
+                .and_then(|armor_item| match armor_item.0 {
+                    ArmorType::Artifact(artifact_armor_id, armor, attunement) => {
+                        if let Some(personal) = attunement {
+                            let amount = match armor.base_armor().weight_class() {
+                                ArmorWeightClass::Light => 4,
+                                ArmorWeightClass::Medium => 5,
+                                ArmorWeightClass::Heavy => 6,
+                            };
+
+                            Some((
+                                MoteCommitmentId::AttunedArtifact(ArtifactId::Armor(
+                                    artifact_armor_id,
+                                )),
+                                MoteCommitment {
+                                    name: armor.name(),
+                                    peripheral: amount - personal.min(amount),
+                                    personal: personal.min(amount),
+                                },
+                            ))
+                        } else {
+                            None
+                        }
+                    }
+                    ArmorType::Mundane(_, _) => None,
+                })
+        });
+
+        let wonder_commitments = self.wonders.iter().filter_map(|wonder_id| {
+            self.wonders.get(wonder_id).and_then(|wonder| {
+                if let Some(personal) = wonder.2 {
+                    if let Some(amount) = wonder.1.attunement_cost {
+                        Some((
+                            MoteCommitmentId::AttunedArtifact(ArtifactId::Wonder(wonder.0)),
+                            MoteCommitment {
+                                name: wonder.name(),
+                                peripheral: amount - personal.min(amount),
+                                personal: personal.min(amount),
+                            },
+                        ))
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            })
+        });
+
+        other_commitments
+            .chain(weapon_commitments)
+            .chain(armor_commitments)
+            .chain(wonder_commitments)
     }
 }
