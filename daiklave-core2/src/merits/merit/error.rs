12dc5ecf5e158e5eac0e92426851429287a9d8ec
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-01-23 14:56:04 -0800

    Flaw error docs

--- daiklave-core2/src/merits/merit/error.rs
+++ daiklave-core2/src/merits/merit/error.rs
@@ -3,8 +3,8 @@ use thiserror::Error;
 /// An error relating to Merits.
 #[derive(Debug, Error)]
 pub enum MeritError {
-    /// Can't have two merits with the same Id
-    #[error("Can't have two merits with the same Id")]
+    /// Can't have two merits with the same Id or two flaws with the same name
+    #[error("Can't have two merits with the same Id, or two Flaws with the same name")]
     DuplicateMerit,
     /// All Exalts must have Exalted Healing
     #[error("Exalted Healing is required for all Exalts")]
@@ -12,8 +12,8 @@ pub enum MeritError {
     /// Merit templates require at least one valid dot rating.
     #[error("At least one valid rating required")]
     MissingDotRating,
-    /// Can't find that merit
-    #[error("Merit not found")]
+    /// Can't find that merit or flaw
+    #[error("Merit/Flaw not found")]
     NotFound,
     /// Merit templates can only define dot ratings from 0 to 5, and instances
     /// can only select from these available ratings.
