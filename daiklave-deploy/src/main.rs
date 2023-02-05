0efae5d09f76a2df2caa7e9d1c2fd65dc095225d
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-02-05 12:51:58 -0800

    Deploy command to dev server

--- daiklave-deploy/src/main.rs
+++ daiklave-deploy/src/main.rs
@@ -1,66 +1,89 @@
-use serde_json::{json, Value};
-use serenity::builder::CreateCommand;
+use std::collections::HashMap;
+
+use serde_json::{Value};
+use serenity::{builder::CreateCommand, all::CommandType};
 
 const DISCORD_API_URL_BASE: &str = "https://discord.com/api/v10/";
 
-async fn create_app_commands() {
+fn main() {
     dotenvy::dotenv().unwrap();
 
-    // Register all application commands in the dev server.
     // Get an access token so that we can update commands
-    let http_client = reqwest::Client::new();
+    let http_client = reqwest::blocking::Client::new();
 
     let client_secret = std::env::var("DISCORD_CLIENT_SECRET")
         .expect("Expected DISCORD_CLIENT_SECRET in environment");
     let application_id =
         std::env::var("DISCORD_TOKEN").expect("Expected DISCORD_TOKEN in environment");
     let token_url = format!("{}{}", DISCORD_API_URL_BASE, "/oauth2/token");
-    let payload = json!({
-        "grant_type": "client_credentials",
-        "scope": "identify applications.commands.update",
-    });
+    let mut payload = HashMap::new();
+    payload.insert("grant_type", "client_credentials");
+    payload.insert("scope", "applications.commands.update");
 
-    let access_token = http_client
-        .post(&token_url)
-        .json(&payload)
-        .basic_auth(&client_secret, Some(&application_id))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .send()
-        .await
-        .expect("Expected client credentials request to succeed")
-        .json::<Value>()
-        .await
-        .expect("Expected client credentials response to be json")
+    let request = http_client
+    .post(&token_url)
+    .form(&payload)
+    .basic_auth(&application_id, Some(&client_secret))
+    .build()
+    .unwrap();
+
+    println!("{:?}", request.headers());
+    println!("{}", std::str::from_utf8(request.body().unwrap().as_bytes().unwrap()).unwrap());
+
+    let json_response = http_client.execute(request)
+    .expect("Expected client credentials request to succeed")
+    .json::<Value>()
+    .expect("Expected client credentials response to be json");
+
+    println!("{}", json_response);
+
+    let access_token = json_response
         .get("access_token")
         .expect("Expected client credentials response to include an access token")
-        .to_string();
+        .as_str()
+        .expect("Expected client credentials response to include an access token");
+
+    println!("{:?}", access_token);
 
     // Use that token to set application commands
+    // let global_application_commands_url = format!(
+    //     "{}{}{}{}",
+    //     DISCORD_API_URL_BASE,
+    //     "applications/",
+    //     application_id,
+    //     "/commands"
+    // );
+    // println!("{}", &global_application_commands_url);
+
     let dev_guild_id =
         std::env::var("DISCORD_TEST_GUILD").expect("Expected DISCORD_TEST_GUILD in environment");
 
     let guild_application_commands_url = format!(
         "{}{}{}{}{}{}",
         DISCORD_API_URL_BASE,
-        "/applications/",
+        "applications/",
         application_id,
         "/guilds/",
         dev_guild_id,
         "/commands"
     );
+    println!("{}", &guild_application_commands_url);
 
-    let commands =
-        vec![CreateCommand::new("version").description("Returns the Daiklave version being used")];
 
-    http_client
+    let guild_commands =
+        vec![CreateCommand::new("version").description("Returns the Daiklave version being used").kind(CommandType::ChatInput)];
+    let request = http_client
         .put(&guild_application_commands_url)
         .bearer_auth(access_token)
-        .json(&commands)
-        .send()
-        .await
-        .expect("Expected to be able to update dev guild app commands");
-}
+        .json(&guild_commands)
+        .build()
+        .unwrap();
 
-fn main() {
-    futures::executor::block_on(create_app_commands());
+    println!("{:?}", request.headers());
+    println!("{}", std::str::from_utf8(request.body().unwrap().as_bytes().unwrap()).unwrap());
+
+    let response = http_client.execute(request).unwrap();
+
+    println!("{}", response.status());
+    println!("{}", response.text().unwrap());
 }
