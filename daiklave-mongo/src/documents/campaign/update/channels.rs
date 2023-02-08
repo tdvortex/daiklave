eb22d6a51c4be6d014860fc9254c7dceec34c781
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-02-07 17:13:02 -0800

    Modify channel update to specify invalidations

--- daiklave-mongo/src/documents/campaign/update/channels.rs
+++ daiklave-mongo/src/documents/campaign/update/channels.rs
@@ -2,11 +2,11 @@ use std::collections::HashSet;
 
 use bson::{doc, oid::ObjectId, Bson};
 use futures::TryStreamExt;
-use serenity::all::ChannelId;
+use serenity::all::{ChannelId, UserId};
 
 use crate::{
-    campaign::CampaignDocument,
-    channel::{ChannelCurrent, ChannelDocument, ChannelVersion, CreateChannel},
+    campaign::{CampaignCurrent, CampaignDocument},
+    channel::{ChannelCurrent, ChannelVersion, CreateChannel},
     error::DocumentError,
     user::UserDocument,
 };
@@ -26,18 +26,32 @@ pub struct UpdateCampaignChannels {
 
 impl UpdateCampaignChannels {
     /// Updates the channel list for a campaign. This requires a session to
-    /// update campaigns, users, and channels atomically.
+    /// update campaigns, users, and channels atomically. Returns a vector of
+    /// pairs of (channel_id, player_id) permissions which must be invalidated
+    /// in the cache.
     pub async fn execute(
         &self,
         database: &mongodb::Database,
         session: &mut mongodb::ClientSession,
-    ) -> Result<(), DocumentError> {
+    ) -> Result<Vec<(ChannelId, UserId)>, DocumentError> {
         session.start_transaction(None).await?;
 
+        // Get the existing campaign document, extract its current players and channels
+        let campaigns = database.collection::<CampaignCurrent>("campaigns");
+        let filter = doc! {
+            "_id": self._id
+        };
+        let campaign = campaigns
+            .find_one_with_session(filter, None, session)
+            .await?
+            .ok_or(DocumentError::NotFound)?;
+        let campaign_players = campaign.players.clone();
+        let old_channels = campaign.channels.clone();
+        let new_channels = self.channels.clone();
+
         // Check to make sure none of the channels are in use by a diffent campaign
-        let channels = database.collection::<ChannelDocument>("channels");
-        let channel_ids: Vec<Bson> = self
-            .channels
+        let channels = database.collection::<ChannelCurrent>("channels");
+        let channel_ids: Vec<Bson> = old_channels
             .iter()
             .map(|channel_id| bson::to_bson(channel_id).or(Err(DocumentError::SerializationError)))
             .fold(
@@ -60,34 +74,57 @@ impl UpdateCampaignChannels {
             .find_with_session(filter, None, session)
             .await?
             .stream(session)
-            .try_collect::<Vec<ChannelDocument>>()
+            .try_collect::<Vec<ChannelCurrent>>()
             .await?
             .into_iter()
-            .map(|document| ChannelCurrent::from(document))
             .any(|current| current.campaign_id != self._id);
         if any_channels_in_use {
             return Err(DocumentError::DuplicateChannelCampaign);
         }
 
-        // Drop all existing channel documents for this campaign
+        // Identify channels to add and drop
+        let drop_channels = old_channels
+            .difference(&new_channels)
+            .copied()
+            .collect::<Vec<ChannelId>>();
+        let add_channels = new_channels
+            .difference(&old_channels)
+            .copied()
+            .collect::<Vec<ChannelId>>();
+
+        // Drop the removed channels
+        let drop_channels_bson = drop_channels
+            .iter()
+            .map(|channel_id| bson::to_bson(channel_id).or(Err(DocumentError::SerializationError)))
+            .fold(
+                Ok(Vec::new()),
+                |acc: Result<Vec<Bson>, DocumentError>, res_bson| match (acc, res_bson) {
+                    (Ok(mut v), Ok(b)) => {
+                        v.push(b);
+                        Ok(v)
+                    }
+                    (Err(e), _) => Err(e),
+                    (_, Err(e)) => Err(e),
+                },
+            )?;
         let query = doc! {
-            "campaignId": self._id
+            "channelId": {
+                "$in": drop_channels_bson
+            }
         };
         channels
             .delete_many_with_session(query, None, session)
             .await?;
 
         // Instantiate new channel documents for this campaign
-        let mut new_channels = Vec::new();
-
-        for channel in self.channels.iter() {
-            let new_channel = CreateChannel {
+        let new_channels = add_channels
+            .into_iter()
+            .map(|channel_id| CreateChannel {
                 version: ChannelVersion::V0,
-                channel_id: *channel,
+                channel_id,
                 campaign_id: self._id,
-            };
-            new_channels.push(new_channel);
-        }
+            })
+            .collect::<Vec<CreateChannel>>();
         let channels = database.collection::<CreateChannel>("channels");
         channels
             .insert_many_with_session(new_channels, None, session)
@@ -131,7 +168,17 @@ impl UpdateCampaignChannels {
             .update_many_with_session(query, update, options, session)
             .await?;
 
+        // Done with the database
         session.commit_transaction().await?;
-        Ok(())
+
+        // Build the set of channel-player pairs that is no longer valid for this campaign
+        Ok(drop_channels
+            .into_iter()
+            .flat_map(|channel_id| {
+                campaign_players
+                    .iter()
+                    .map(move |&discord_id| (channel_id, discord_id))
+            })
+            .collect::<Vec<(ChannelId, UserId)>>())
     }
 }
