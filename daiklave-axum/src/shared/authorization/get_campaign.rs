c6d63275a61d632938219c47d04a4e353a5c231f
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-02-11 09:10:51 -0800

    Implement cache population for campaign auth

--- daiklave-axum/src/shared/authorization/get_campaign.rs
+++ daiklave-axum/src/shared/authorization/get_campaign.rs
@@ -20,7 +20,7 @@ impl GetCampaignAuthorization {
     async fn execute_mongo(
         &self,
         database: &mongodb::Database,
-    ) -> Result<Option<Authorization>, DataError> {
+    ) -> Result<Option<UserCurrent>, DataError> {
         let users = database.collection::<UserCurrent>("users");
         let user_id_bson = bson::to_bson(&self.user_id)
             .or_else(|_| Err(DataError::SerializationError(format!("{:?}", self.user_id))))?;
@@ -31,29 +31,31 @@ impl GetCampaignAuthorization {
             }
         };
 
-        let user = if let Some(user) = users.find_one(filter, None).await? {
-            user
-        } else {
-            // The database successfully returned no user with this Id and campaign
-            return Ok(None);
-        };
+        Ok(users.find_one(filter, None).await?)
 
-        let campaign = if let Some(campaign) = user
-            .campaigns
-            .iter()
-            .find(|player_campaign| player_campaign.campaign_id == self.campaign_id)
-        {
-            campaign
-        } else {
-            // This shouldn't happen, but if we get a user that doesn't have this campaign treat them as unauthorized
-            return Ok(None);
-        };
+        // let user = if let Some(user) = users.find_one(filter, None).await? {
+        //     user
+        // } else {
+        //     // The database successfully returned no user with this Id and campaign
+        //     return Ok(None);
+        // };
 
-        Ok(Some(Authorization {
-            user_id: self.user_id,
-            campaign_id: campaign.campaign_id,
-            is_storyteller: campaign.is_storyteller,
-        }))
+        // let campaign = if let Some(campaign) = user
+        //     .campaigns
+        //     .iter()
+        //     .find(|player_campaign| player_campaign.campaign_id == self.campaign_id)
+        // {
+        //     campaign
+        // } else {
+        //     // This shouldn't happen, but if we get a user that doesn't have this campaign treat them as unauthorized
+        //     return Ok(None);
+        // };
+
+        // Ok(Some(Authorization {
+        //     user_id: self.user_id,
+        //     campaign_id: campaign.campaign_id,
+        //     is_storyteller: campaign.is_storyteller,
+        // }))
     }
 
     async fn execute_redis<CON>(
@@ -69,13 +71,17 @@ impl GetCampaignAuthorization {
         let mut field = "campaignId:".as_bytes().to_vec();
         field.extend(self.campaign_id.bytes());
 
-        let maybe_is_storyteller: Option<bool> = connection.hget(key, field).await?;
-        if let Some(is_storyteller) = maybe_is_storyteller {
-            Ok(Some(Authorization {
-                user_id: self.user_id,
-                campaign_id: self.campaign_id,
-                is_storyteller,
-            }))
+        let maybe_value_bytes: Option<Vec<u8>> = connection.hget(key, field).await?;
+        if let Some(value_bytes) = maybe_value_bytes {
+            if let Some(is_storyteller) = value_bytes.get(0).map(|byte| byte == &u8::from(true)) {
+                Ok(Some(Authorization {
+                    user_id: self.user_id,
+                    campaign_id: self.campaign_id,
+                    is_storyteller,
+                }))
+            } else {
+                Ok(None)
+            }
         } else {
             Ok(None)
         }
@@ -97,19 +103,58 @@ impl GetCampaignAuthorization {
             return Ok(Some(authorization));
         }
 
-        // Either a connection error with Redis or a cache miss, try to get from Mongo
-        let maybe_authorization = match self.execute_mongo(database).await {
-            Ok(maybe_authorization) => maybe_authorization,
-            Err(e) => {
-                // Can't access source-of-truth, throw 500 or equivalent message to Discord
-                return Err(e);
-            }
-        };
+        // Either a connection error with Redis or a cache miss, try to get user from Mongo
+        let maybe_user = self.execute_mongo(database).await?;
 
-        if let Some(authorization) = maybe_authorization {
-            // Found authorization in MongoDb; backfill the cache
-            // TODO
-            Ok(Some(authorization))
+        if let Some(user) = maybe_user {
+            // Found the user in MongoDb with this campaign
+            // Get the campaign out of the user
+            let campaign = if let Some(campaign) = user
+                .campaigns
+                .iter()
+                .find(|player_campaign| player_campaign.campaign_id == self.campaign_id)
+            {
+                campaign
+            } else {
+                // This shouldn't happen, but if we get a user that doesn't 
+                // have this campaign treat them as unauthorized and don't 
+                // update cache
+                return Ok(None);
+            };
+
+            // Populate the user's permissions hash set in Redis
+            // Key = "userId:" + big-endian bytes of their Discord snowflake
+            // Campaign field = "campaignId" + bytes of the campaign's ObjectId
+            // Campaign value = vec![u8::from(is_storyteller)], to have a consistent data type
+            // Channel field(s) = "channelId" + big-endian bytes of the channel's Discord snowflake
+            // Channel value(s) = bytes of the campaign's ObjectId, extended with [u8::from(is_storyteller)
+            let is_storyteller = campaign.is_storyteller;
+
+            let mut key = "userId:".as_bytes().to_vec();
+            key.extend(self.user_id.0.get().to_be_bytes());
+
+            let mut items = Vec::new();
+            let mut campaign_field = "campaignId:".as_bytes().to_vec();
+            campaign_field.extend(self.campaign_id.bytes());
+
+            let campaign_value = vec![u8::from(campaign.is_storyteller)];
+            items.push((campaign_field, campaign_value));
+
+            for channel in campaign.channels.iter() {
+                let mut channel_field = "channelId:".as_bytes().to_vec();
+                channel_field.extend(channel.0.get().to_be_bytes());
+
+                let mut channel_value = self.campaign_id.bytes().to_vec();
+                channel_value.push(u8::from(is_storyteller));
+
+                items.push((channel_field, channel_value));
+            }
+
+            // We don't need the redis result (or even for it to succeed)
+            let _: Result<Vec<Vec<u8>>, redis::RedisError> = connection.hset_multiple(key, &items).await;
+
+            // Return the completed auth
+            Ok(Some(Authorization { user_id: self.user_id, campaign_id: campaign.campaign_id, is_storyteller }))
         } else {
             // The user is not authorized for this campaign
             Ok(None)
