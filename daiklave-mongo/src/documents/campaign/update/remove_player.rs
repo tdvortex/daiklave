54ee3a664e350e3e1ab5e15795c565b18bf449e9
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-02-10 16:06:20 -0800

    Implement remove player and set ST

--- daiklave-mongo/src/documents/campaign/update/remove_player.rs
+++ daiklave-mongo/src/documents/campaign/update/remove_player.rs
@@ -1,8 +1,8 @@
-use bson::oid::ObjectId;
+use bson::{oid::ObjectId, doc};
 use mongodb::ClientSession;
 use serenity::all::UserId;
 
-use crate::error::DocumentError;
+use crate::{error::DocumentError, campaign::CampaignCurrent, user::UserCurrent, character::CharacterCurrent};
 
 /// Removes a player from a campaign. The storyteller cannot be removed.
 pub struct RemoveCampaignPlayer {
@@ -16,6 +16,46 @@ impl RemoveCampaignPlayer {
     /// Removes the player from the campaign. Requires a session to update 
     /// campaigns, players, and characters atomically.
     pub async fn execute(&self, database: &mongodb::Database, session: &mut ClientSession) -> Result<(), DocumentError> {
-        todo!()
+        session.start_transaction(None).await?;
+
+        let player_bson = bson::to_bson(&self.user_id).or(Err(DocumentError::SerializationError))?;
+
+        // Update the campaign document to no longer have this player
+        let campaigns = database.collection::<CampaignCurrent>("campaigns");
+        let query = doc! {
+            "_id": self.campaign_id
+        };
+        let update = doc! {
+            "$pull": {
+                "players": &player_bson
+            }
+        };
+        campaigns.update_one_with_session(query, update, None, session).await?;
+
+        // Remove the campaign from the player
+        let users = database.collection::<UserCurrent>("users");
+        let query = doc! {
+            "discordId": &player_bson
+        };
+        let update = doc! {
+            "$pull": {
+                "campaigns": {
+                    "campaignId": self.campaign_id
+                }
+            }
+        };
+        users.update_one_with_session(query, update, None, session).await?;
+
+        // Remove all characters this player has with this campaign
+        let characters = database.collection::<CharacterCurrent>("characters");
+        let query = doc! {
+            "player": &player_bson,
+            "campaignId": self.campaign_id
+        };
+        characters.delete_many_with_session(query, None, session).await?;
+
+        // Done with database
+        session.commit_transaction().await?;
+        Ok(())
     }
 }
--- daiklave-mongo/src/documents/campaign/update/set_storyteller.rs
+++ daiklave-mongo/src/documents/campaign/update/set_storyteller.rs
@@ -1,7 +1,7 @@
-use bson::oid::ObjectId;
+use bson::{oid::ObjectId, doc};
 use serenity::all::UserId;
 
-use crate::error::DocumentError;
+use crate::{error::DocumentError, campaign::CampaignCurrent, user::{UserCurrent, CreateUser, UserVersion}, PlayerCampaign};
 
 /// Reassigns the storyteller for this campaign. The storyteller document will 
 /// be created if it does not exist, and will be added to the campaign if not
@@ -18,6 +18,80 @@ impl SetStoryteller {
     /// Reassigns the storyteller. Requires a session to update campaign and 
     /// players atomically.
     pub async fn execute(&self, database: &mongodb::Database, session: &mut mongodb::ClientSession) -> Result<(), DocumentError> {
-        todo!()
+        session.start_transaction(None).await?;
+
+        let player_bson = bson::to_bson(&self.user_id).or(Err(DocumentError::SerializationError))?;
+
+        // Get the campaign document
+        let campaigns = database.collection::<CampaignCurrent>("campaigns");
+        let filter = doc! {
+            "_id": self.campaign_id
+        };
+        let mut campaign = campaigns.find_one_with_session(filter, None, session).await?.ok_or(DocumentError::NotFound)?;
+
+        if campaign.storyteller == self.user_id {
+            // Nothing to do if player is already storyteller
+            session.commit_transaction().await?;
+            return Ok(());
+        }
+
+        // Update the campaign to have this player as both a player and storyteller
+        campaign.players.insert(self.user_id);
+        campaign.storyteller = self.user_id;
+        let query = doc! {
+            "_id": self.campaign_id
+        };
+        campaigns.replace_one_with_session(query, &campaign, None, session).await?;
+
+        // Get the player document if it exists
+        let users = database.collection::<UserCurrent>("users");
+        let filter = doc! {
+            "discordId": &player_bson
+        };
+        let maybe_user = users.find_one_with_session(filter, None, session).await?;
+
+        if let Some(mut user) = maybe_user {
+            if let Some(existing_campaign) = user.campaigns.iter_mut().find(|player_campaign| player_campaign.campaign_id == self.campaign_id) {
+                // Update the existing campaign document to have is_storyteller = true
+                existing_campaign.is_storyteller = true;
+            } else {
+                // Add the campaign if it doesn't already exist
+                let added_campaign = PlayerCampaign {
+                    campaign_id: self.campaign_id,
+                    name: campaign.name.clone(),
+                    is_storyteller: true,
+                    characters: Default::default(),
+                    dice_channel: campaign.dice_channel,
+                    channels: campaign.channels,
+                };
+                user.campaigns.push(added_campaign);
+            }
+
+            // Replace the existing user document
+            let query = doc! {
+                "discordId": &player_bson
+            };
+            users.replace_one_with_session(query, user, None, session).await?;
+        } else {
+            // Add a new user document
+            let user = CreateUser {
+                version: UserVersion::V0,
+                discord_id: self.user_id,
+                campaigns: vec![PlayerCampaign {
+                    campaign_id: self.campaign_id,
+                    name: campaign.name.clone(),
+                    is_storyteller: true,
+                    characters: Default::default(),
+                    dice_channel: campaign.dice_channel,
+                    channels: campaign.channels,
+                }]
+            };
+            let users = database.collection::<CreateUser>("users");
+            users.insert_one_with_session(user, None, session).await?;
+        }
+
+        // Done with database
+        session.commit_transaction().await?;
+        Ok(())
     }
-}
+} 
