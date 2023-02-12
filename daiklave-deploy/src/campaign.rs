b011dbc7bd7cbec1086713a11df8ac23465634d4
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-02-11 16:22:31 -0800

    Add campaign slash commands (with todos)

--- daiklave-deploy/src/campaign.rs
+++ daiklave-deploy/src/campaign.rs
@@ -1,7 +1,37 @@
 use serenity::{builder::{CreateCommand, CreateCommandOption}, all::{CommandOptionType, CommandType}};
 
 pub fn campaign() -> CreateCommand {
-    let campaign_create = CreateCommandOption::new(
+    // TODO
+    let channels_help = CreateCommandOption::new(
+        CommandOptionType::SubCommand, 
+        "help", 
+        "Help with campaign channels commands"
+    );
+
+    // TODO
+    let channels_set = CreateCommandOption::new(
+        CommandOptionType::SubCommand,
+        "set",
+        "Set the channels used in this campaign"
+    );
+
+    // TODO
+    let channels_show = CreateCommandOption::new(
+        CommandOptionType::SubCommand,
+        "show",
+        "Show the channels used by this campaign"
+    );
+
+    // TODO
+    let channels = CreateCommandOption::new(
+        CommandOptionType::SubCommandGroup,
+        "channels",
+        "Campaign channel commands"
+    ).add_sub_option(channels_help)
+    .add_sub_option(channels_set)
+    .add_sub_option(channels_show);
+
+    let create = CreateCommandOption::new(
         CommandOptionType::SubCommand,
         "create",
         "Create a new campaign",
@@ -14,10 +44,51 @@ pub fn campaign() -> CreateCommand {
         ).required(true);
         campaign_name.min_length(1);
         campaign_name
-    });    
+    });
+
+    // TODO
+    let help = CreateCommandOption::new(
+        CommandOptionType::SubCommand,
+        "help",
+        "Help with campaign commands"
+    );
+
+    // TODO
+    let join = CreateCommandOption::new(
+        CommandOptionType::SubCommand,
+        "join",
+        "Join this campaign"
+    );
+
+    // TODO
+    let kick = CreateCommandOption::new(
+        CommandOptionType::SubCommand,
+        "kick",
+        "Kick a player from the campaign (Storyteller only)"
+    );
+
+    // TODO
+    let leave = CreateCommandOption::new(
+        CommandOptionType::SubCommand,
+        "leave",
+        "Leave this campaign"
+    );
+
+    // TODO
+    let rename = CreateCommandOption::new(
+        CommandOptionType::SubCommand,
+        "rename",
+        "Rename this campaign (Storyteller only)"
+    );
     
     CreateCommand::new("campaign")
-        .description("Campaign management commands")
+        .description("Campaign commands")
         .kind(CommandType::ChatInput)
-        .add_option(campaign_create)
+        .add_option(channels)
+        .add_option(create)
+        .add_option(help)
+        .add_option(join)
+        .add_option(kick)
+        .add_option(leave)
+        .add_option(rename)
 }
