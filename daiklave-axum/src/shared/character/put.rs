c08d96ecaf59dc63aa1a73acc66a0469c5a16dca
Ben Wilson <ben.aiken.wilson@gmail.com>
2023-02-13 18:01:41 -0800

    Implement put character

--- daiklave-axum/src/shared/character/put.rs
+++ daiklave-axum/src/shared/character/put.rs
@@ -1,9 +1,9 @@
 use daiklave_core::CharacterMemo;
-use mongodb::{bson::oid::ObjectId, ClientSession};
+use mongodb::{bson::{oid::ObjectId, doc}, ClientSession};
 use redis::AsyncCommands;
 use serenity::all::UserId;
 
-use crate::shared::error::DatabaseError;
+use crate::{shared::{error::DatabaseError, to_bson}, mongo::{characters::CharacterCurrent, users::UserCurrent}};
 
 /// Fully replaces an exiting character. This is mostly used from the Yew
 /// frontend to bundle multiple operations together.
@@ -19,24 +19,68 @@ pub struct PutCharacter {
 }
 
 impl PutCharacter {
-    async fn execute_mongo(&self,
-        _database: &mongodb::Database, 
-        _session: &mut ClientSession) -> Result<(), DatabaseError> {
-        todo!()
+    async fn execute_mongo(
+        &self,
+        database: &mongodb::Database,
+        session: &mut ClientSession,
+    ) -> Result<(), DatabaseError> {
+        session.start_transaction(None).await?;
+
+        // Replace the character document
+        let characters = database.collection::<CharacterCurrent>("characters");
+        let query = doc! {
+            "_id": self.character_id,
+            "player": to_bson(&self.player)?,
+            "campaignId": self.character_id,
+        };
+        let replacement = CharacterCurrent {
+            _id: self.character_id,
+            player: self.player,
+            campaign_id: self.campaign_id,
+            character: self.character.clone(),
+        };
+        let replace_result = characters.replace_one_with_session(query, replacement, None, session).await?;
+        if replace_result.matched_count < 1 {
+            return Err(DatabaseError::NotFound("Character".to_owned()));
+        }
+
+        // Update the name in the player document
+        let users = database.collection::<UserCurrent>("users");
+        let query = doc! {
+            "discordId": to_bson(&self.player)?,
+            "campaigns": {
+                "campaignId": self.campaign_id,
+                "characters.character.characterId": self.character_id
+            }
+        };
+        let update = doc! {
+            "$set": {
+                "campaigns.$.characters.character.$.name": self.character.name.clone(),
+            }
+        };
+        users.update_one_with_session(query, update, None, session).await?;
+
+        // Done with database
+        session.commit_transaction().await?;
+        Ok(())
     }
 
     async fn execute_redis<CON: AsyncCommands>(
         &self,
-        _connection: &mut CON,
+        connection: &mut CON,
     ) -> Result<(), DatabaseError> {
-        todo!()
+        let mut key = "characterId:".as_bytes().to_vec();
+        key.extend(self.character_id.bytes());
+
+        connection.del(vec![key]).await?;
+        Ok(())
     }
 
     /// Overwrites the existing character in the database, updates the name of
     /// the character in the player's subdocument, and invalidates the cache.
     pub async fn execute<CON: AsyncCommands>(
-        &self, 
-        database: &mongodb::Database, 
+        &self,
+        database: &mongodb::Database,
         session: &mut ClientSession,
         connection: &mut CON,
     ) -> Result<(), DatabaseError> {
